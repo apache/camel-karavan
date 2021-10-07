a6630fe7f96bd607665beab2b2b81fb07300c3a3
Marat Gubaidullin <marat.gubaidullin@gmail.com>
2021-10-07 12:01:42 -0400

    Fixed #7. Apache License headers (#8)

--- karavan-vscode/src/extension.ts
+++ karavan-vscode/src/extension.ts
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import * as vscode from "vscode";
 import * as fs from "fs";
 import * as path from "path";
@@ -128,7 +144,5 @@ function isIntegration(yaml: string): [boolean, string?] {
     }
 }
 
-// this method is called when your extension is deactivated
-// eslint-disable-next-line @typescript-eslint/no-empty-function
 export function deactivate() {
 }
