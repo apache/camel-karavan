25fd0e3cacfe405a7c649ac970839fcc3474e0f0
Marat Gubaidullin <marat.gubaidullin@gmail.com>
2021-10-08 10:30:26 -0400

    Implements #12

--- karavan-designer/src/designer/ui/DslElement.tsx
+++ karavan-designer/src/designer/ui/DslElement.tsx
@@ -26,6 +26,8 @@ import {CamelApi} from "../api/CamelApi";
 import {CamelUi} from "../api/CamelUi";
 import {EventBus} from "../api/EventBus";
 import {DslPath} from "./DslPath";
+import {CamelApiExt} from "../api/CamelApiExt";
+import {Languages} from "../api/CamelMetadata";
 
 interface Props {
     step: CamelElement,
@@ -102,11 +104,55 @@ export class DslElement extends React.Component<Props, State> {
         return this.state.step.dslName.startsWith("from");
     }
 
+    isShowTooltip = (): boolean => {
+        const exp = CamelApiExt.getExpressionValue(this.state.element);
+        return this.state.element.hasOwnProperty("expression") && (exp !== undefined && exp?.trim().length > 0);
+    }
+
+    getExpressionTooltip = (): string => {
+        const language = CamelApiExt.getExpressionLanguage(this.state.element) || 'simple';
+        const value = CamelApiExt.getExpressionValue(this.state.element) || '';
+        return language.concat(": ", value);
+    }
+
     getArrow = () => {
         return (
             <img className={"arrow-down"} alt="arrow"
                  src="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' version='1.1' xmlns:xlink='http://www.w3.org/1999/xlink' xmlns:svgjs='http://svgjs.com/svgjs' x='0' y='0' viewBox='0 0 512 512' style='enable-background:new 0 0 512 512' xml:space='preserve' class=''%3E%3Cg transform='matrix(1,0,0,1,1.7053025658242404e-13,1.1368683772161603e-13)'%3E%3Cg xmlns='http://www.w3.org/2000/svg'%3E%3Cg%3E%3Cpath d='M374.108,373.328c-7.829-7.792-20.492-7.762-28.284,0.067L276,443.557V20c0-11.046-8.954-20-20-20 c-11.046,0-20,8.954-20,20v423.558l-69.824-70.164c-7.792-7.829-20.455-7.859-28.284-0.067c-7.83,7.793-7.859,20.456-0.068,28.285 l104,104.504c0.006,0.007,0.013,0.012,0.019,0.018c7.792,7.809,20.496,7.834,28.314,0.001c0.006-0.007,0.013-0.012,0.019-0.018 l104-104.504C381.966,393.785,381.939,381.121,374.108,373.328z' fill='%23e97826' data-original='%23000000' style='' class=''%3E%3C/path%3E%3C/g%3E%3C/g%3E%3C/g%3E%3C/svg%3E"
             />)
+    }
+
+    getHeader = () => {
+        return (
+            <div className="header"
+                 style={
+                     ["choice", "multicast"].includes(this.state.element.dslName)
+                         ? {width: "100%", borderWidth: this.isSelected() ? "2px" : "1px"}
+                         : {borderWidth: this.isSelected() ? "2px" : "1px"}
+                 }
+                 ref={el => {
+                     if (el && (this.state.step.dslName === 'fromStep' || this.state.step.dslName === 'toStep')) EventBus.sendPosition(this.state.step, el.getBoundingClientRect());
+                 }}>
+                <img draggable={false}
+                     src={CamelUi.getIcon(this.state.element)}
+                     className="icon" alt="icon">
+                </img>
+                <Text>{CamelUi.getTitle(this.state.element)}</Text>
+                <button type="button" aria-label="Delete" onClick={e => this.delete(e)}
+                        className="delete-button">
+                    <DeleteIcon noVerticalAlign/>
+                </button>
+            </div>
+        );
+    }
+
+    getHeaderWithTooltip = () => {
+        return (
+            <Tooltip position={"auto"} disabled={this.isShowTooltip()}
+                     content={<div>{this.getExpressionTooltip()}</div>}>
+                {this.getHeader()}
+            </Tooltip>
+        );
     }
 
     render() {
@@ -123,25 +169,7 @@ export class DslElement extends React.Component<Props, State> {
                  onClick={event => this.selectElement(event)}
 
             >
-                <div className="header"
-                     style={
-                         ["choice", "multicast"].includes(this.state.element.dslName)
-                             ? {width:"100%", borderWidth: this.isSelected() ? "2px" : "1px"}
-                             : {borderWidth: this.isSelected() ? "2px" : "1px"}
-                     }
-                     ref={el => {
-                         if (el && (this.state.step.dslName === 'fromStep' || this.state.step.dslName === 'toStep')) EventBus.sendPosition(this.state.step, el.getBoundingClientRect());
-                     }}>
-                    <img draggable={false}
-                         src={CamelUi.getIcon(this.state.element)}
-                         className="icon" alt="icon">
-                    </img>
-                    <Text>{CamelUi.getTitle(this.state.element)}</Text>
-                    <button type="button" aria-label="Delete" onClick={e => this.delete(e)}
-                            className="delete-button">
-                        <DeleteIcon noVerticalAlign/>
-                    </button>
-                </div>
+                {this.isShowTooltip() ? this.getHeaderWithTooltip() : this.getHeader()}
                 {this.state.element.hasSteps() && !this.horizontal() && this.getArrow()}
                 <div className={this.state.element.dslName}>
                     {this.state.element.hasSteps() &&
