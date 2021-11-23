2eed2be9856afa41accca1d2aee2a3630bfeab8a
Marat Gubaidullin <marat.gubaidullin@gmail.com>
2021-11-22 20:17:01 -0500

    Fixed #106 (#118)

---
+++ change_version.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Set Generator pom.xml version: $1";
+mvn versions:set -DnewVersion=$1 -f karavan-generator
+
+echo "Set Application pom.xml version: $1";
+mvn versions:set -DnewVersion=$1 -f karavan-app
+
+echo "Set Application package.json extension version: $1";
+yarn version --new-version $1 --no-commit --no-git-tag-version --cwd karavan-app/src/main/webapp/
+
+echo "Set VSCode extension package.json version: $1";
+yarn version --new-version $1 --no-commit --no-git-tag-version --cwd karavan-vscode
+
+echo "Set Designer package.json version: $1";
+yarn version --new-version $1 --no-commit --no-git-tag-version --cwd karavan-designer
