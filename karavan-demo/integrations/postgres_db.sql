db8cd29e74a391c585d79d28a298ef9676fadb35
Marat Gubaidullin <marat.gubaidullin@gmail.com>
2022-02-07 15:26:56 -0500

    Demo Integrations

---
+++ karavan-demo/integrations/postgres_db.sql
@@ -0,0 +1,12 @@
+
+CREATE TABLE IF NOT EXISTS table1 (
+  id INT NOT NULL,
+  name varchar(250) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS table2 (
+  id varchar(250) NOT NULL,
+  name varchar(250) NOT NULL,
+  PRIMARY KEY (id)
+);
