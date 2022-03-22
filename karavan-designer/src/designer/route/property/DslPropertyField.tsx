0e6c58da16badb70b677ec2526d75e5d052f998e
Marat Gubaidullin <marat.gubaidullin@gmail.com>
2022-03-22 13:20:21 -0400

    Update DslPropertyField.tsx

--- karavan-designer/src/designer/route/property/DslPropertyField.tsx
+++ karavan-designer/src/designer/route/property/DslPropertyField.tsx
@@ -180,14 +180,14 @@ export class DslPropertyField extends React.Component<Props, State> {
     }
 
     getSwitch = (property: PropertyMeta, value: any) => {
-        const isChecked = value ? Boolean(value) === true : Boolean(property.defaultValue);
+        const isChecked = value != undefined ? Boolean(value) : Boolean(property.defaultValue);
         return (
             <Switch
                 id={property.name} name={property.name}
                 value={value?.toString()}
                 aria-label={property.name}
                 isChecked={isChecked}
-                onChange={e => this.propertyChanged(property.name, !Boolean(value))}/>
+                onChange={e => this.propertyChanged(property.name, e)}/>
         )
     }
 
