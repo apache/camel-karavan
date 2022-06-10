7d4f8d89871d26ff1e05b2b4666a2c21432fccfb
Marat Gubaidullin <marat.gubaidullin@gmail.com>
2022-06-10 11:15:27 -0400

    Simplified properties

--- karavan-core/src/core/api/ProjectModelApi.ts
+++ karavan-core/src/core/api/ProjectModelApi.ts
@@ -14,61 +14,22 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import {ProjectModel, ProjectStatus, ProjectProperty} from "../model/ProjectModel";
+import {ProjectModel, ProjectProperty} from "../model/ProjectModel";
 import {v4 as uuidv4} from "uuid";
-
-const PREFIX_JBANG = "camel.jbang";
-const PREFIX_MAIN = "camel.main";
 
 export class ProjectModelApi {
 
     static propertiesToProject = (properties: string): ProjectModel => {
-        const allLines = properties.split(/\r?\n/).filter(text => text.trim().length > 0 && !text.trim().startsWith("#"));
-        const managedLines = allLines.filter(text => text.startsWith(PREFIX_JBANG) || text.startsWith(this.getKeyWithPrefix('routesIncludePattern')));
-        const unmanagedLines = allLines.filter(text => !managedLines.includes(text));
-        const map = this.propertiesToMap(managedLines);
+        const lines = properties.split(/\r?\n/).filter(text => text.trim().length > 0 && !text.trim().startsWith("#"));
         const project = new ProjectModel();
-        project.name = this.getValue(map, "project.name");
-        project.version = this.getValue(map, "project.version");
-        project.namespace = this.getValue(map, "project.namespace");
-        project.cleanup = this.getValue(map, "project.cleanup") === "true";
-        const image = this.getValue(map, "build.image.image", "deploy.image");
-        project.image = image ? image : project.namespace + "/" + project.name + ":" + project.version;
-        project.classpathFiles = this.getValue(map, "classpathFiles");
-        project.routesIncludePattern = this.getValue(map, "routesIncludePattern");
-        project.sourceImage = this.getValue(map, "build.image.source-image");
-        project.from = this.getValue(map, "build.image.from");
-        project.buildConfig = this.getValue(map, "build.image.build-config") === "true";
 
-        project.filename = this.getValue(map, "build.image.jar", "package.uber-jar.jar");
-        project.replicas = this.getValue(map, "deploy.replicas");
-        project.nodePort = this.getValue(map, "deploy.node-port");
-        project.server = this.getValue(map, "deploy.server", "build.image.server");
-        project.username = this.getValue(map, "deploy.username", "build.image.username");
-        const openshift = this.getValue(map, "deploy.openshift", "build.image.openshift") === "true";
-        const minikube = this.getValue(map, "deploy.minikube", "build.image.minikube") === "true";
-        project.target = openshift ? "openshift" : (minikube ? "minikube" : "kubernetes");
-        project.deploy = this.getValue(map, "deploy") === "true";
-        project.build = this.getValue(map, "build.image") === "true";
-        project.uberJar = this.getValue(map, "package") === "true";
-        project.manifests = this.getValue(map, "manifests") === "true";
-        project.path = this.getValue(map, "manifests.path");
-        project.status = new ProjectStatus();
-        project.properties = this.propertiesFromMap(this.propertiesToMap(unmanagedLines));
+        project.properties = this.propertiesFromMap(this.propertiesToMap(lines));
 
         Object.keys(project).filter(key => key !== 'properties').forEach(key => {
             const value = (project as any)[key];
             if ( value === undefined || value === 'undefined') delete (project as any)[key];
         })
         return project;
-    }
-
-    static getValue = (map: Map<string, any>, ...keys: string[]): any => {
-        for (const key of keys) {
-            const k = this.getKeyWithPrefix(key);
-            if  (map.has(k)) return map.get(k);
-        }
-        return undefined;
     }
 
     static propertiesToMap = (properties: string[]): Map<string, any> => {
@@ -102,64 +63,10 @@ export class ProjectModelApi {
 
     static projectToMap = (project: ProjectModel): Map<string, any> => {
         const map = new Map<string, any>();
-        if (project.image?.length === 0) project.image = project.namespace + "/" + project.name + ":" + project.version;
 
         if (project.properties && project.properties.length >0){
             project.properties.forEach(p => map.set(p.key, p.value));
         }
-        this.setValue(map, "project.name", project.name);
-        this.setValue(map, "project.version", project.version);
-        this.setValue(map, "project.namespace", project.namespace);
-        this.setValue(map, "project.cleanup", project.cleanup);
-        this.setValue(map, "package", project.uberJar);
-        this.setValue(map, "package.uber-jar.jar", project.filename);
-        this.setValue(map, "package.uber-jar.fresh", true);
-        this.setValue(map, "classpathFiles", project.classpathFiles);
-        this.setValue(map, "routesIncludePattern", project.routesIncludePattern);
-        this.setValue(map, "build.image", project.build);
-        this.setValue(map, "build.image.openshift", project.target === 'openshift');
-        this.setValue(map, "build.image.build-config", project.buildConfig);
-        this.setValue(map, "build.image.push", project.target === 'openshift' && !project.buildConfig);
-        this.setValue(map, "build.image.jar", project.filename);
-        this.setValue(map, "build.image.image", project.image);
-        this.setValue(map, "build.image.source-image", project.sourceImage);
-        this.setValue(map, "build.image.from", project.from);
-        this.setValue(map, "build.image.server", project.server);
-        this.setValue(map, "build.image.username", project.username);
-        this.setValue(map, "deploy", project.deploy);
-        this.setValue(map, "deploy.openshift", project.target === 'openshift');
-        this.setValue(map, "deploy.minikube", project.target === 'minikube');
-        this.setValue(map, "deploy.image", project.image);
-        this.setValue(map, "deploy.replicas", project.replicas);
-        this.setValue(map, "deploy.node-port", project.nodePort);
-        this.setValue(map, "deploy.server", project.server);
-        this.setValue(map, "deploy.username", project.username);
-        this.setValue(map, "undeploy.openshift", project.target === 'openshift');
-        this.setValue(map, "undeploy.minikube", project.target === 'minikube');
-        this.setValue(map, "undeploy.server", project.server);
-        this.setValue(map, "manifests", project.manifests);
-        this.setValue(map, "manifests.path", project.manifests);
-        this.setValue(map, "manifests.openshift", project.target === 'openshift');
-        this.setValue(map, "manifests.minikube", project.target === 'minikube');
-        this.setValue(map, "manifests.image", project.image);
-        this.setValue(map, "manifests.replicas", project.replicas);
-        this.setValue(map, "manifests.node-port", project.nodePort);
-        this.setValue(map, "manifests.server", project.server);
-        this.setValue(map, "manifests.jar", project.filename);
-
         return map;
-    }
-
-    static setValue = (map: Map<string, any>, key: string, value: any): Map<string, any> => {
-        if (value !== undefined && value?.toString().length > 0)
-            map.set(this.getKeyWithPrefix(key), value);
-        else
-            map.set(this.getKeyWithPrefix(key), undefined);
-        return map;
-    }
-
-    static getKeyWithPrefix = (key: string): string => {
-        const prefix = key === 'routesIncludePattern' ? PREFIX_MAIN : PREFIX_JBANG;
-        return prefix + "." + key;
     }
 }
--- karavan-core/src/core/model/ProjectModel.ts
+++ karavan-core/src/core/model/ProjectModel.ts
@@ -16,88 +16,22 @@
  */
 import {v4 as uuidv4} from "uuid";
 
-export class StepStatus {
-    status: 'pending' | 'progress' | 'done' | 'error' = 'pending';
-    startTime: number = Date.now();
-    endTime?: number;
-
-    public constructor(init?: Partial<StepStatus>) {
-        Object.assign(this, init);
-    }
-
-    static progress(): StepStatus {
-        return new StepStatus({status: "progress", startTime: Date.now()})
-    }
-
-    static done(stepStatus?: StepStatus): StepStatus | undefined {
-        if (stepStatus){
-            stepStatus.status = "done";
-            stepStatus.endTime = Date.now();
-        }
-        return stepStatus
-    }
-
-    static error(stepStatus?: StepStatus): StepStatus | undefined {
-        if (stepStatus) {
-            stepStatus.status = "error";
-            stepStatus.endTime = Date.now();
-        }
-        return stepStatus
-    }
-}
-
 export class ProjectProperty {
     id: string = ''
     key: string = ''
+    profile?: string
     value: any
 
     public constructor(init?: Partial<ProjectProperty>) {
         Object.assign(this, init);
     }
 
-    static createNew(key: string, value: any): ProjectProperty {
-        return new ProjectProperty({id: uuidv4(), key: key, value: value})
-    }
-}
-
-export class ProjectStatus extends StepStatus{
-    uberJar?: StepStatus;
-    build?: StepStatus;
-    deploy?: StepStatus;
-    undeploy?: StepStatus;
-    active: boolean = false;
-
-    public constructor(init?: Partial<ProjectStatus>) {
-        super();
-        Object.assign(this, init);
+    static createNew(key: string, value: any, profile?: string): ProjectProperty {
+        return new ProjectProperty({id: uuidv4(), key: key, value: value, profile: profile})
     }
 }
 
 export class ProjectModel {
-    name: string = 'demo'
-    version: string = '1.0.0'
-    filename: string = 'camel-runner.jar'
-    namespace: string = 'default'
-    cleanup: boolean = false
-    image?: string = this.namespace + "/" + this.name + ":" + this.version
-    sourceImage: string = 'java:openjdk-11-ubi8'
-    from: string = 'gcr.io/distroless/java:11'
-    replicas: number = 1
-    nodePort: number = 30777
-    server?: string
-    username?: string
-    password?: string
-    token?: string
-    target: 'openshift' | 'minikube' | 'kubernetes' = 'minikube'
-    deploy: boolean = false
-    build: boolean = false
-    buildConfig: boolean = false
-    uberJar: boolean = true
-    manifests: boolean = true
-    path: string = ''
-    classpathFiles: string = ''
-    routesIncludePattern: string = ''
-    status: ProjectStatus = new ProjectStatus()
     properties: ProjectProperty[] = []
 
     public constructor(init?: Partial<ProjectModel>) {
