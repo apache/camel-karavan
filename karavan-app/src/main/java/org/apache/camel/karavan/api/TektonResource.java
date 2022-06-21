b0c0a2805d03fe2c99da184267d5a131bcc8f551
Marat Gubaidullin <marat.gubaidullin@gmail.com>
2022-06-21 19:54:56 -0400

    Saas feature8 (#386)
    * PVC for Pod
    
    * Tekton client and API

---
+++ karavan-app/src/main/java/org/apache/camel/karavan/api/TektonResource.java
@@ -0,0 +1,54 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.karavan.api;
+
+import org.apache.camel.karavan.model.Project;
+import org.apache.camel.karavan.model.ProjectFile;
+import org.apache.camel.karavan.service.InfinispanService;
+import org.apache.camel.karavan.service.KubernetesService;
+import org.jboss.logging.Logger;
+
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+@Path("/tekton")
+public class TektonResource {
+
+    @Inject
+    InfinispanService infinispanService;
+
+    @Inject
+    KubernetesService kubernetesService;
+
+    private static final Logger LOGGER = Logger.getLogger(TektonResource.class.getName());
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Project push(@HeaderParam("username") String username, Project project) throws Exception {
+        Project p = infinispanService.getProject(project.getProjectId());
+        List<ProjectFile> files = infinispanService.getProjectFiles(project.getProjectId());
+        String pipelineRunId = kubernetesService.createPipelineRun(project);
+        return p;
+    }
+}
