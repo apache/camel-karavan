4864877e0c7fb0dd30b89d68d462069248450ffc
Marat Gubaidullin <marat.gubaidullin@gmail.com>
2022-07-26 11:41:11 -0400

    Sync versioning with Camel (#427)

--- change_version.sh
+++ change_version.sh
@@ -6,11 +6,15 @@ mvn versions:set -DnewVersion=$1 -f karavan-generator
 echo "Set Application pom.xml version: $1";
 mvn versions:set -DnewVersion=$1 -f karavan-app
 
+echo "Set Core package.json extension version: $1";
+yarn version --new-version $1 --no-commit --no-git-tag-version --cwd karavan-core
+
+echo "Set Designer package.json version: $1";
+yarn version --new-version $1 --no-commit --no-git-tag-version --cwd karavan-designer
+
 echo "Set Application package.json extension version: $1";
 yarn version --new-version $1 --no-commit --no-git-tag-version --cwd karavan-app/src/main/webapp/
 
 echo "Set VSCode extension package.json version: $1";
 yarn version --new-version $1 --no-commit --no-git-tag-version --cwd karavan-vscode
 
-echo "Set Designer package.json version: $1";
-yarn version --new-version $1 --no-commit --no-git-tag-version --cwd karavan-designer
