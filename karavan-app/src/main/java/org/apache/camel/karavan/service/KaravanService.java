4c17d43e53fe579e42129103b90d0f4c87a3d30d
Marat Gubaidullin <marat.gubaidullin@gmail.com>
2022-12-15 14:20:44 -0500

    Turn on informers

--- karavan-app/src/main/java/org/apache/camel/karavan/service/KaravanService.java
+++ karavan-app/src/main/java/org/apache/camel/karavan/service/KaravanService.java
@@ -49,7 +49,7 @@ public class KaravanService {
         infinispanService.start();
         setEnvironment();
         initialImport();
-//        startInformers();
+        startInformers();
     }
 
     void onStop(@Observes ShutdownEvent ev) {
