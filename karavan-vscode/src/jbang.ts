6aef05dcbfb9692ec2b7709524846eafc1f851ac
Marat Gubaidullin <marat.gubaidullin@gmail.com>
2023-03-31 09:28:54 -0400

    Fix #700

--- karavan-vscode/src/jbang.ts
+++ karavan-vscode/src/jbang.ts
@@ -22,7 +22,7 @@ import * as exec from "./exec";
 import { CamelDefinitionYaml } from "core/api/CamelDefinitionYaml";
 
 export async function camelJbangGenerate(rootPath: string, openApiFullPath: string, fullPath: string, add: boolean, generateRoutes: boolean, generateRest: boolean) {
-    let command = prepareCommand("generate rest -i '" + openApiFullPath + "'");
+    let command = prepareCommand("generate rest -i '" + openApiFullPath + "' --type=yaml ");
     console.log(command);
     if (generateRoutes === true) command = command + " --routes";
     executeJbangCommand(rootPath, command, async (code, stdout, stderr) => {
