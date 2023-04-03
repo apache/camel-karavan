b33b14a9c8ab5c74fccfc98968da65451647647e
Marat Gubaidullin <marat.gubaidullin@gmail.com>
2023-04-03 08:57:17 -0400

    Bump version to 3.20.2-SNAPSHOT

--- karavan-operator/src/main/java/org/apache/camel/karavan/operator/spec/KaravanOperatorCSVMetadata.java
+++ karavan-operator/src/main/java/org/apache/camel/karavan/operator/spec/KaravanOperatorCSVMetadata.java
@@ -21,9 +21,9 @@ package org.apache.camel.karavan.operator.spec;
 
 
 @CSVMetadata(
-        name = "camel-karavan-operator.v3.20.1",
+        name = "camel-karavan-operator.v3.20.2-SNAPSHOT",
         annotations = @CSVMetadata.Annotations(
-                containerImage = "ghcr.io/apache/camel-karavan-operator:3.20.1",
+                containerImage = "ghcr.io/apache/camel-karavan-operator:3.20.2-SNAPSHOT",
                 repository = "https://github.com/apache/camel-karavan",
                 capabilities = "Basic Install",
                 categories = "Developer Tools, Integration & Delivery",
