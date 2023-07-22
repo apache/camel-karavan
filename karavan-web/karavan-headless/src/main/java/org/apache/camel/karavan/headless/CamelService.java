fc9c11fbbebad3c006ee9344f2718bf4f68a8558
Marat Gubaidullin <marat.gubaidullin@gmail.com>
2023-07-21 20:59:25 -0400

    karavan-headless #817

---
+++ karavan-web/karavan-headless/src/main/java/org/apache/camel/karavan/headless/CamelService.java
@@ -0,0 +1,230 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.karavan.headless;
+
+import io.quarkus.vertx.ConsumeEvent;
+import io.vertx.core.json.JsonObject;
+import io.vertx.mutiny.core.Vertx;
+import io.vertx.mutiny.core.buffer.Buffer;
+import io.vertx.mutiny.core.eventbus.EventBus;
+import io.vertx.mutiny.ext.web.client.HttpResponse;
+import io.vertx.mutiny.ext.web.client.WebClient;
+import org.apache.camel.karavan.infinispan.InfinispanService;
+import org.apache.camel.karavan.infinispan.model.CamelStatus;
+import org.apache.camel.karavan.infinispan.model.ContainerStatus;
+import org.apache.camel.karavan.infinispan.model.DevModeStatus;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.eclipse.microprofile.faulttolerance.CircuitBreaker;
+import org.jboss.logging.Logger;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.concurrent.ExecutionException;
+
+import static org.apache.camel.karavan.headless.EventService.CMD_COLLECT_CAMEL_STATUS;
+import static org.apache.camel.karavan.headless.EventService.CMD_DELETE_CAMEL_STATUS;
+
+@ApplicationScoped
+public class CamelService {
+
+    private static final Logger LOGGER = Logger.getLogger(CamelService.class.getName());
+
+    @Inject
+    InfinispanService infinispanService;
+
+    @ConfigProperty(name = "karavan.environment")
+    String environment;
+
+    @Inject
+    Vertx vertx;
+
+    @Inject
+    EventBus eventBus;
+
+    WebClient webClient;
+
+    public WebClient getWebClient() {
+        if (webClient == null) {
+            webClient = WebClient.create(vertx);
+        }
+        return webClient;
+    }
+
+    public void reloadProjectCode(String projectId) {
+        LOGGER.info("Reload project code " + projectId);
+        try {
+            ContainerStatus containerStatus = infinispanService.getDevModeContainerStatuses(projectId, environment);
+            Integer exposedPort = containerStatus.getExposedPort();
+            infinispanService.getProjectFiles(projectId).forEach(projectFile -> putRequest(projectId,exposedPort, projectFile.getName(), projectFile.getCode(), 1000));
+            reloadRequest(projectId, exposedPort);
+            DevModeStatus dms = infinispanService.getDevModeStatus(projectId);
+            dms.setCodeLoaded(true);
+            infinispanService.saveDevModeStatus(dms);
+        } catch (Exception ex) {
+            LOGGER.error(ex.getMessage());
+        }
+    }
+
+    @CircuitBreaker(requestVolumeThreshold = 10, failureRatio = 0.5, delay = 1000)
+    public boolean putRequest(String containerName, Integer exposedPort, String fileName, String body, int timeout) {
+        try {
+            String url = getContainerAddress(containerName, exposedPort) + "/q/upload/" + fileName;
+            HttpResponse<Buffer> result = getWebClient().putAbs(url)
+                    .timeout(timeout).sendBuffer(Buffer.buffer(body)).subscribeAsCompletionStage().toCompletableFuture().get();
+            return result.statusCode() == 200;
+        } catch (Exception e) {
+            LOGGER.info(e.getMessage());
+        }
+        return false;
+    }
+
+    public void reloadRequest(String containerName, Integer exposedPort) {
+        String url = getContainerAddress(containerName, exposedPort) + "/q/dev/reload?reload=true";
+        try {
+            result(url, 1000);
+        } catch (InterruptedException | ExecutionException e) {
+            LOGGER.error(e.getMessage());
+        }
+    }
+
+    public String getContainerAddress(String containerName, Integer exposedPort) {
+        return "http://localhost:" + exposedPort;
+    }
+
+    public void collectCamelStatuses() {
+        if (infinispanService.isReady()) {
+            infinispanService.getContainerStatuses(environment).forEach(pod -> {
+                CamelStatusRequest csr = new CamelStatusRequest(pod.getProjectId(), pod.getName(), pod.getExposedPort());
+                eventBus.publish(CMD_COLLECT_CAMEL_STATUS, JsonObject.mapFrom(csr));
+            });
+        }
+    }
+
+    public void collectCamelStatuses(JsonObject data) {
+        CamelStatusRequest dms = data.mapTo(CamelStatusRequest.class);
+        Arrays.stream(CamelStatus.Name.values()).forEach(statusName -> {
+            String containerName = dms.getContainerName();
+            Integer exposedPort = dms.getExposedPort();
+            String status = getCamelStatus(containerName, exposedPort, statusName);
+            if (status != null) {
+                CamelStatus cs = new CamelStatus(dms.getProjectId(), containerName, statusName, status, environment);
+                infinispanService.saveCamelStatus(cs);
+            }
+        });
+    }
+
+    public void cleanupDevModeStatuses() {
+        if (infinispanService.isReady()) {
+            infinispanService.getDevModeStatuses().forEach(dms -> {
+                ContainerStatus pod = infinispanService.getDevModeContainerStatuses(dms.getProjectId(), environment);
+                if (pod == null) {
+                    eventBus.publish(CMD_DELETE_CAMEL_STATUS, JsonObject.mapFrom(dms));
+                }
+            });
+        }
+    }
+
+    public void cleanupDevModeStatus(JsonObject data) {
+        DevModeStatus dms = data.mapTo(DevModeStatus.class);
+        Arrays.stream(CamelStatus.Name.values()).forEach(name -> {
+            infinispanService.deleteCamelStatus(dms.getProjectId(), name.name(), environment);
+        });
+    }
+
+    private void reloadCode(String podName, String oldContext, String newContext) {
+        String newState = getContextState(newContext);
+        String oldState = getContextState(oldContext);
+        if (newContext != null && !Objects.equals(newState, oldState) && "Started".equals(newState)) {
+            reloadProjectCode(podName);
+        }
+    }
+
+    private String getContextState(String context) {
+        if (context != null) {
+            JsonObject obj = new JsonObject(context);
+            return obj.getJsonObject("context").getString("state");
+        } else {
+            return null;
+        }
+    }
+
+    public String getCamelStatus(String podName, Integer exposedPort, CamelStatus.Name statusName) {
+        String url = getContainerAddress(podName, exposedPort) + "/q/dev/" + statusName.name();
+        try {
+            return result(url, 500);
+        } catch (InterruptedException | ExecutionException e) {
+            LOGGER.error(e.getMessage());
+        }
+        return null;
+    }
+
+    @CircuitBreaker(requestVolumeThreshold = 10, failureRatio = 0.5, delay = 1000)
+    public String result(String url, int timeout) throws InterruptedException, ExecutionException {
+        try {
+            HttpResponse<Buffer> result = getWebClient().getAbs(url).putHeader("Accept", "application/json")
+                    .timeout(timeout).send().subscribeAsCompletionStage().toCompletableFuture().get();
+            if (result.statusCode() == 200) {
+                JsonObject res = result.bodyAsJsonObject();
+                return res.encodePrettily();
+            }
+        } catch (Exception e) {
+            LOGGER.info(e.getMessage());
+        }
+        return null;
+    }
+
+    public static class CamelStatusRequest {
+        private String projectId;
+        private String containerName;
+        private Integer exposedPort;
+
+        public CamelStatusRequest() {
+        }
+
+        public CamelStatusRequest(String projectId, String containerName, Integer exposedPort) {
+            this.projectId = projectId;
+            this.containerName = containerName;
+            this.exposedPort = exposedPort;
+        }
+
+        public String getProjectId() {
+            return projectId;
+        }
+
+        public void setProjectId(String projectId) {
+            this.projectId = projectId;
+        }
+
+        public String getContainerName() {
+            return containerName;
+        }
+
+        public void setContainerName(String containerName) {
+            this.containerName = containerName;
+        }
+
+        public Integer getExposedPort() {
+            return exposedPort;
+        }
+
+        public void setExposedPort(Integer exposedPort) {
+            this.exposedPort = exposedPort;
+        }
+    }
+}
---
+++ karavan-web/karavan-headless/src/main/java/org/apache/camel/karavan/headless/HeadlessService.java
@@ -0,0 +1,54 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.karavan.headless;
+
+import io.quarkus.runtime.StartupEvent;
+import io.quarkus.scheduler.Scheduled;
+import org.apache.camel.karavan.infinispan.InfinispanService;
+import org.jboss.logging.Logger;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
+import javax.inject.Inject;
+
+@ApplicationScoped
+public class HeadlessService {
+
+    private static final Logger LOGGER = Logger.getLogger(HeadlessService.class.getName());
+
+    @Inject
+    InfinispanService infinispanService;
+
+    @Inject
+    CamelService camelService;
+
+    void onStart(@Observes StartupEvent ev) {
+        LOGGER.info("Starting Headless Karavan");
+        infinispanService.start(true);
+        infinispanService.clearAllStatuses();
+    }
+
+    @Scheduled(every = "{karavan.camel.status.pull-interval}", concurrentExecution = Scheduled.ConcurrentExecution.SKIP)
+    void collectCamelStatuses() {
+        LOGGER.info("Collect info statuses");
+        // collect Camel statuses
+        camelService.collectCamelStatuses();
+        // clean DevMode statuses if container deleted
+        camelService.cleanupDevModeStatuses();
+    }
+
+}
