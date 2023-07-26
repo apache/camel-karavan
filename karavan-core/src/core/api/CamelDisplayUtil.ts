59a1aa4330711e90529a9ab7b38b6d8493d1ccc3
Filip Ambrož <filip.ambroz@physter.com>
2023-07-26 07:51:21 +0200

    Removed js extension from import in src

--- karavan-core/src/core/api/CamelDisplayUtil.ts
+++ karavan-core/src/core/api/CamelDisplayUtil.ts
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { Integration, CamelElement } from '../model/IntegrationDefinition.js';
+import { Integration, CamelElement } from '../model/IntegrationDefinition';
 import { CamelUtil } from './CamelUtil';
 import { CamelDefinitionApi } from './CamelDefinitionApi';
 import { CamelDefinitionApiExt } from './CamelDefinitionApiExt';
