aa99df10e46b88ea63f6a3f969d2ed96584d8585
Marat Gubaidullin <marat@talismancloud.io>
2023-09-28 17:17:15 -0400

    Fix #915

--- karavan-designer/src/designer/route/useRouteDesignerHook.tsx
+++ karavan-designer/src/designer/route/useRouteDesignerHook.tsx
@@ -77,15 +77,16 @@ export function useRouteDesignerHook () {
 
     const deleteElement = () =>  {
         EventBus.sendPosition("clean", new CamelElement(""), undefined, new DOMRect(), new DOMRect(), 0);
+        let i = integration;
         selectedUuids.forEach(uuidToDelete => {
-            const i = CamelDefinitionApiExt.deleteStepFromIntegration(integration, uuidToDelete);
-            setIntegration(i, false);
-            setShowSelector(false);
-            setShowDeleteConfirmation(false);
-            setDeleteMessage('');
-            setSelectedStep(undefined);
-            setSelectedUuids([]);
+             i = CamelDefinitionApiExt.deleteStepFromIntegration(i, uuidToDelete);
         });
+        setIntegration(i, false);
+        setShowSelector(false);
+        setShowDeleteConfirmation(false);
+        setDeleteMessage('');
+        setSelectedStep(undefined);
+        setSelectedUuids([]);
     }
 
     const selectElement = (element: CamelElement) =>  {
