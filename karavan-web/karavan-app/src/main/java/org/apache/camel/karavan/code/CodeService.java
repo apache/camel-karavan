e70d6be53c6a0088163b85eb7be2768ef8c26736
Marat Gubaidullin <marat@talismancloud.io>
2023-10-20 21:29:02 -0400

    apicurio.datamodels changes in camel 4.1 #931

--- karavan-web/karavan-app/src/main/java/org/apache/camel/karavan/code/CodeService.java
+++ karavan-web/karavan-app/src/main/java/org/apache/camel/karavan/code/CodeService.java
@@ -16,7 +16,6 @@
  */
 package org.apache.camel.karavan.code;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import io.apicurio.datamodels.Library;
