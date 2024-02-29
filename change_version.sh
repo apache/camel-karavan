a9747f5c486f5d12f15d4b36a3a64170e375e111
Marat Gubaidullin <marat@talismancloud.io>
2024-02-29 17:56:52 -0500

    Bump version to 4.4.1

--- change_version.sh
+++ change_version.sh
@@ -6,8 +6,6 @@ mvn versions:set -DnewVersion=$1 -f karavan-generator
 echo "Set Application pom.xml version: $1";
 mvn versions:set -DnewVersion=$1 -f karavan-app
 
-
-
 npm config set sign-git-tag false
 npm config set commit-hooks false
 npm config set workspaces-update false
@@ -28,8 +26,6 @@ npm version --new-version $1 --prefix  karavan-vscode
 echo "Set VSCode extension package.json version: $1";
 npm version --new-version $1 --prefix  karavan-space
 
-
-
 echo "Set Github Workflow Devmode TAG version: $1";
 sed -i.bak 's/TAG:.*/TAG: '"$1"'/g' .github/workflows/docker-devmode.yml 
 
@@ -37,7 +33,7 @@ echo "Set Github Workflow App TAG version: $1";
 sed -i.bak 's/TAG:.*/TAG: '"$1"'/g' .github/workflows/app.yml 
 
 echo "Set Github Workflow App TAG version: $1";
-sed -i.bak 's/TAG:.*/TAG: '"$1"'/g' .github/workflows/installer.yml
+sed -i.bak 's/TAG:.*/TAG: '"$1"'/g' .github/workflows/app-oidc.yml 
 
 echo "Set application.properties karavan.version=$1";
 sed -i.bak 's/karavan.version.*/karavan.version='"$1"'/g' karavan-app/src/main/resources/application.properties 
