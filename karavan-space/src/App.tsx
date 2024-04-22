86a80c08a1d3de8b36f6384663764ef7b15628b7
Marat Gubaidullin <marat@talismancloud.io>
2024-04-22 09:22:02 -0400

    Sync with designer

--- karavan-space/src/App.tsx
+++ karavan-space/src/App.tsx
@@ -187,6 +187,7 @@ class App extends React.Component<Props, State> {
                         onClickAddRoute={() => {}}
                         onClickAddREST={() => {}}
                         onClickAddBean={() => {}}
+                        onClickAddKamelet={() => {}}
                         hideToolbar={true}
                     />
                 )
