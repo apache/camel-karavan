dce5221a57b1d69eab81627e50de1af6acd41b08
jvandborg <16362036+jvandborg@users.noreply.github.com>
2024-07-10 13:35:55 +0200

    Extended change_version.sh

--- change_version.sh
+++ change_version.sh
@@ -23,6 +23,9 @@ npm version --new-version $1 --prefix  karavan-app/src/main/webui/
 echo "Set VSCode extension package.json version: $1";
 npm version --new-version $1 --prefix  karavan-vscode
 
+echo "Set README.md camel.jbang.version=$1";
+sed -i.bak 's/camel.jbang.version=[0-9].[0-9].[0-9]/camel.jbang.version='"$1"'/g' karavan-vscode/README.md 
+
 echo "Set VSCode extension package.json version: $1";
 npm version --new-version $1 --prefix  karavan-space
 
@@ -40,3 +43,24 @@ sed -i.bak 's/TAG:.*/TAG: '"$1"'/g' .github/workflows/vscode.yml
 
 echo "Set application.properties karavan.version=$1";
 sed -i.bak 's/karavan.version.*/karavan.version='"$1"'/g' karavan-app/src/main/resources/application.properties 
+
+echo "Set application.properties camel-karavan-devmode:$1";
+sed -i.bak 's/camel-karavan-devmode.*/camel-karavan-devmode:'"$1"'/g' karavan-app/src/main/resources/application.properties 
+
+echo "Set docker-compose-gitea.yaml camel-karavan:$1";
+sed -i.bak 's/camel-karavan.*/camel-karavan:'"$1"'/g' docs/install/karavan-docker/docker-compose-gitea.yaml 
+
+echo "Set docker-compose.yaml camel-karavan:$1";
+sed -i.bak 's/camel-karavan.*/camel-karavan:'"$1"'/g' docs/install/karavan-docker/docker-compose.yaml 
+
+echo "Set VSCODE_HOWTO.md camel.jbang.version=$1";
+sed -i.bak 's/camel.jbang.version=[0-9].[0-9].[0-9]/camel.jbang.version='"$1"'/g' docs/VSCODE_HOWTO.md 
+
+echo "Set deployment.yaml camel-karavan-devmode:$1";
+sed -i.bak 's/camel-karavan-devmode:[0-9].[0-9].[0-9]/camel-karavan-devmode:'"$1"'/g' docs/install/karavan-kubernetes/deployment.yaml
+
+echo "Set deployment.yaml camel-karavan:$1";
+sed -i.bak 's/camel-karavan:[0-9].[0-9].[0-9]/camel-karavan:'"$1"'/g' docs/install/karavan-kubernetes/deployment.yaml
+
+echo "Set kustomization.yaml camel-karavan:$1";
+sed -i.bak 's/app.kubernetes.io\/version.*/app.kubernetes.io\/version: "'"$1"'"/g' docs/install/karavan-kubernetes/kustomization.yaml
